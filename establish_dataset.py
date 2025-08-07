f605f8a303a083584204cbfff2813b752b9b270b
Sebastian Dayer <dayseb77@gmail.com>
2025-08-07 10:37:22 -0500

    Update establish_dataset.py

--- establish_dataset.py
+++ establish_dataset.py
@@ -12,3 +12,15 @@ data_dir = pathlib.Path('/home/dayseb/.keras/datasets/sebastian_pictures')
 # Get amount of images in directory
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split = 0.2,
+  subset = "training",
+  seed = 123
+  image_size = (img_height, img_width),
+  batch_size = batch_size)
