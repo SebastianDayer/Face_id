51b85acd2da0744c3ff72621e1ad709490457469
SebastianDayer <dayseb77@gmail.com>
2025-08-07 17:29:10 +0100

    Update

--- establish_dataset.py
+++ establish_dataset.py
@@ -7,8 +7,11 @@ import pathlib
 
 # Establish the directory from which images are pulled
 
-data_dir = pathlib.Path('/home/dayseb/.keras/datasets/sebastian_pictures')
+image_dir = pathlib.Path('/home/dayseb/.keras/datasets/face_detection_photos')
+data_dir = image_dir.glob('*/*.jpg')
 
 # Get amount of images in directory
 
-image_count = len(list(data_dir.glob('*/*.jpg')))
+image_count = len(list(data_dir))
+
+print(image_count)
